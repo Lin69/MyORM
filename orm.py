f91d0572e10025bf438f6f59b8cd16e41e984a91
Lin Smith <linasmith69@yahoo.com>
2019-03-31 13:11:25 +0300

    Made my best for now
    Didn't implement lots of features I wanted and quite sure it dosn't wark as it was supposed to

--- orm.py
+++ orm.py
@@ -1,44 +1,7 @@
 import psycopg2
-from collections import defaultdict
-
-conn = psycopg2.connect(
-    database = "test",
-    user="postgres",
-    password="lina69",
-    host="localhost",
-    port="5432"
-)
-cor = conn.cursor()
-
-# TableList = []
-# AttrNames = {}
-# defaultdict(list)
-
-# def connet():
-#     global TableList
-#     cor.execute("select table_name from information_schema.tables where table_schema='public';")
-#     TableList = [k[0] for k in cor]
-
-#     cor.execute("select table_name, column_name from information_schema.columns where table_schema='public';")
-
-#     global AttrNames
-
-#     for k, v in cor:
-#         if k in AttrNames.keys():
-#             AttrNames[k].append(v)
-#         else:
-#             alst = [v]
-#             AttrNames = {k:alst}
-    
-
-# connet()
-
-# print('tab', TableList)
-# print ('attr', AttrNames)
 
 class Field():
     
-    # todo unique
     def __init__(self,f_type,required=False,default=None):
         self.f_type = f_type
         self.required = required
@@ -55,7 +18,6 @@ class Field():
         if not self.f_type(value):
             raise TypeError(f"Value '{value}' cannot be converted to type '{self.f_type}'")
         return self.f_type(value)
-    
 
 class IntField(Field):
     
@@ -64,8 +26,127 @@ class IntField(Field):
 
 class StringField(Field):
 
-        def __init__(self, required=False, default=None):
-            super().__init__(str,required, default)
+    def __init__(self, required=False, default=None):
+        super().__init__(str,required, default)
+    
+
+class connection():
+    
+    def __init__(self,*_, database,user="postgres", password="postgres", host="localhost", port="5432"):
+
+        conn = psycopg2.connect(
+            database = database,
+            user=user,
+            password=password,
+            host=host,
+            port=port
+        )
+        self.cur = conn.cursor()
+        self.table_cash = {}
+        
+        self.cur.execute(f"select table_name, column_name,data_type from information_schema.columns where table_schema='public';")
+
+        for i, k, v in self.cur:
+            if i not in self.table_cash.keys():
+                dicl={k:v}
+                self.table_cash[i]=dicl
+            else:
+                self.table_cash[i][k]=v
+
+    def create_table(self,*_,table_name,attrs):
+        if table_name not in self.table_cash.keys():
+            st = 'CREATE TABLE '+table_name+' ('
+            stlist = []
+            for key, value in attrs.items():
+                ss = key
+                if isinstance(value,IntField):
+                    typ = 'integer'
+                elif isinstance(value,StringField):
+                    typ = 'text'
+                else: 
+                    typ = value
+                ss += ' '+ typ
+                stlist.append(ss)
+            st += ', '.join(stlist)
+            st += ');'
+            self.cur.execute(st)
+            self.table_cash[table_name]=attrs
+
+        else:
+            raise ValueError(f"Table {table_name} is already exists")
+
+    def delete_table(self,*_, table_name, attrs):
+
+        if table_name in self.table_cash.keys():
+            st = 'DROP TABLE '+table_name+";"
+            self.cur.execute(st)
+        else:
+            raise ValueError(f"Table {table_name} doesn't exist")
+
+    def add_attr(self,*_,table_name,attrs):
+
+        if table_name in self.table_cash.keys():
+            stlist = []
+            st = 'ALTER TABLE '+table_name
+            for key, value in attrs.items():
+                if key not in self.table_cash[table_name]:
+                    if isinstance(value,IntField):
+                        typ = 'integer'
+                    elif isinstance(value,StringField):
+                        typ = 'text'
+                    else: 
+                        typ = value
+                        ss = " ADD COLUMN " + key + " "+ typ
+                        stlist.append(ss)
+                    self.table_cash[table_name][key]=value
+                else: 
+                    raise ValueError(f"Attibute {key} already exists")
+            st += ", ".join(stlist)
+            st += ';'
+            self.cur.execute(st)
+        else:
+            raise ValueError(f"Table {table_name} doesn't exist")
+
+    def drop_attr(self,*args, table_name):
+        
+        if table_name in self.table_cash.keys():
+            stlist = []
+            st = 'ALTER TABLE '+table_name
+            for key in args:
+                if key in self.table_cash[table_name]:
+                    ss = " DROP COLUMN IF EXISTS " + key
+                    stlist.append(ss)
+                    del self.table_cash[table_name][key]
+                else: 
+                    raise ValueError(f"Attibute {key} doesn't exist")
+            st += ", ".join(stlist)
+            st += ';'
+            self.cur.execute(st)
+        else:
+            raise ValueError(f"Table {table_name} doesn't exist")
+
+    def change_table(self,*_,table_name,attrs):
+
+        if table_name in self.table_cash.keys():
+            stlist = []
+            st = 'ALTER TABLE '+table_name
+            for key, value in attrs.items():
+                if key in self.table_cash[table_name]:
+                    ss = " ALTER COLUMN " + key + " TYPE "+ value
+                    stlist.append(ss)
+                    self.table_cash[table_name][key]=value
+                else: 
+                    raise ValueError(f"Attibute {key} doesn't exist")
+            st += ", ".join(stlist)
+            st += ';'
+            self.cur.execute(st)
+        else:
+            raise ValueError(f"Table {table_name} doesn't exist")
+
+bd = connection(
+    database="test",
+    password="lina69"
+)
 
 class Manage:
     def __init__(self):
@@ -76,13 +157,68 @@ class Manage:
             self.model_cls = owner
         return self
 
-    def create(self, **kwargs):
-        print(self.model_cls._fields)
+    def create(self, bd, **kwargs):
+    # Здесь я хотела сделать проверку, на уникальность значений, но в прошлый раз запуталась
+    #
+        st = 'INSERT INTO ' + self.model_cls._table_name + " "
+        stkeys=[]
+        stvalues=[]
+        for k, v in kwargs.items():
+            print(k)
+            if k not in self.model_cls._fields:
+                raise ValueError("No such attribute")
+            else: 
+                stkeys.append(k)
+                if isinstance(self.model_cls._fields[k],IntField):
+                    stvalues.append(v)
+                else:
+                    val = "'"+v+"'"
+                    stvalues.append(val)
+        st+="(" + ", ".join(stkeys) + ") VALUES "
+        st+="(" + ", ".join(stvalues) + ");"
+        bd.cur.execute(st)
 
-    def delete(self, **kwargs):
-        pass
-        # todo
-    
+    def all(self, bd):
+        bd.cur.execute(f"SELECT * FROM {self.model_cls._table_name}")
+        for str in bd.cur:
+            print (str)
+    #Должен ли он сохранять данные? 
+    #выводить данные как словарь атрибут - значение? 
+
+    def delete(self, bd, **kwargs):
+        st = 'DELETE FROM '+self.model_cls._table_name+" WHERE "
+        stlist = []
+        for k, v in kwargs.items():
+            if k not in self.model_cls._fields:
+                raise ValueError(f"There is no attribute '{k}' in table {self.model_cls._table_name}")
+            else:
+                if isinstance(self.model_cls._fields[k],IntField):
+                    ss = k + " = "+ str(v)
+                else:
+                    ss = k + " = '"+ str(v) + "'"
+                stlist.append(ss)
+        st += " and ".join(stlist) + ";"
+        bd.cur.execute(st)
+
+    def update(self, bd, checkdict={}, valuedict={}):
+        st = "UPDATE " + self.model_cls._table_name + " SET "
+        stlist=[]
+        for k, v in valuedict.items():
+            if isinstance(self.model_cls._fields[k],IntField):
+                ss = k + " = "+ str(v)
+            else:
+                ss = k + " = '"+ str(v) + "'"
+            stlist.append(ss)
+        st += ', '.join(stlist) + " WHERE "
+        stlist = []
+        for k, v in checkdict.items():
+            if isinstance(self.model_cls._fields[k],IntField):
+                ss = k + " = "+ str(v)
+            else:
+                ss = k + " = '"+ str(v) + "'"
+            stlist.append(ss)
+        st += ' AND '.join(stlist) + ";"
+        bd.cur.execute(st)
 
 
 class MetaModel(type): 
@@ -102,58 +238,18 @@ class MetaModel(type):
         fields = {k: v for k, v in namespace.items()
             if isinstance(v, Field)}
 
+        # !!!!!!!!!!!!!!!!
+        # attrs = {namespace[k] for k in fields.keys()}
         namespace['_fields'] = fields
         namespace['_table_name'] = meta.table_name
 
-        # Способ 1
-        # if meta.table_name not in TableList:
-        #     print (meta.table_name, 'not in the list')
-        #     todo запрос на создание таблицы
-        # else:
-        #     for f in namespace['_fields'].keys():
-        #         if f not in AttrNames[namespace['_table_name']]:
-        #             print(f, "not in the list of attributes")
-        
-        # Способ 2
-        cor.execute("select table_name from information_schema.tables where table_schema='public';")
-        TableList = [k[0] for k in cor]
+        for base in bases:
+            for key, value in base.__dict__.items():
+                if isinstance(value, Field):
+                    namespace['_fields'][key]=value
 
-        if meta.table_name not in TableList:
-            st = 'CREATE TABLE '+namespace['_table_name']+' ('
-            stlist = []
-            for i in namespace['_fields'].keys():
-                ss = i
-                if isinstance(namespace[i],IntField):
-                    typ = 'integer'
-                else: 
-                    # isinstance(namespace[f],StringField):
-                    typ = 'text'
-                ss += ' '+ typ
-                stlist.append(ss)
-            st += ', '.join(stlist)
-            st += ');'
-            print(st)
-
-            # todo Запрос на создание таблицы
-        else:
-            print (f"table {meta.table_name} is in the list")
-
-            cor.execute(f"select column_name from information_schema.columns where table_schema='public' and table_name = '{namespace['_table_name']}';")
-            
-            AttrNames = [k[0] for k in cor]
-            print(AttrNames)
-            
-            for f in namespace['_fields'].keys():
-                if f not in AttrNames:
-                    if isinstance(namespace[f],IntField):
-                        typ = 'integer'
-                    else: 
-                        # isinstance(namespace[f],StringField):
-                        typ = 'text'
-                    print(f"ALTER TABLE {namespace['_table_name']} ADD COLUMN {f} {typ};")
-                    # raise AttributeError(f"Attribute {f} is absent in the Database")
-                    #todo запрос на дабвление атрибута
-                    
+        if meta.table_name not in bd.table_cash.keys():
+            bd.create_table(table_name=meta.table_name,attrs=namespace['_fields'])
 
         return super().__new__(mcs, name, bases, namespace)
 
@@ -163,9 +259,15 @@ class Model(metaclass=MetaModel):
         table_name = ""
 
     objects = Manage()
-    # objects = Manage(Meta.table_name)
 
+    def __init__(self, *_, **kwargs):
+        # self.objects.create(bd,kwargs)
+        for field_name, field in self._fields.items():
+            value = field.validate(kwargs.get(field_name))
+            setattr(self, field_name, value)
+        
 class MyClass(Model):
+
     id = IntField()
     onemore = StringField();
 
@@ -173,11 +275,17 @@ class MyClass(Model):
         table_name = ""
 
 class courses(Model):
+    
     id = IntField()
-    lala = StringField()
+    cour = StringField()
 
     class Meta: 
         table_name =""
-    
 
-MyClass.objects.create()
+class python(courses):
+    teacher = StringField()
+
+    class Meta:
+        table_name=""
+
+meow = python(id=1,cour='advanced',teacher='cool') 
